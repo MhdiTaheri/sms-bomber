9ef91a4fd5c200e10034583d6802af7bf1cbbb8b
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2023-12-05 23:11:18 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-main() {
-    sudo apt update
-    sudo apt install git pip -y
-    git clone https://github.com/MhdiTaheri/sms-bomber.git
-    (cd sms-bomber && python3 main.py)
-}
-
-main
+sudo apt update
+sudo apt install git pip -y
+git clone https://github.com/MhdiTaheri/sms-bomber.git
+clear
+(cd sms-bomber && python3 main.py)
