6322fb7b1dce9a9a2cee349b86a8b9f029c9cafb
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2023-12-06 09:48:47 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
+# Update and install necessary packages
 sudo apt update
-sudo apt install git pip -y
+sudo apt install git python3-pip -y
+
+# Clone the repository
 git clone https://github.com/MhdiTaheri/sms-bomber.git
 clear
-echo "
 
+# Print ASCII art in green color
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "${GREEN}"
+echo "
 █▀▄▀█ ▄▀█ █░█ █▀▄ █   ▀█▀ ▄▀█ █░█ █▀▀ █▀█ █
 █░▀░█ █▀█ █▀█ █▄▀ █   ░█░ █▀█ █▀█ ██▄ █▀▄ █
 "
+echo -e "${NC}"
+
+# Run the Python script
 (cd sms-bomber && python3 main.py)
